05f0acb234017f0505564ba591dbd68a733c987c
Barun Debnath <barundebnath91@gmail.com>
2022-02-07 23:06:58 +0530

    enter bio page done

--- client/pages/bio.js
+++ client/pages/bio.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { DiGithubBadge } from "react-icons/di";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { IoMdDocument } from "react-icons/io";
+import { CgWebsite } from "react-icons/cg";
 
 const bio = () => {
   return (
     <div>
-      <section class="h-screen bg-gray-100 bg-opacity-50">
+      <section class="h-screen bg-gray-100 bg-opacity-50 mt-10 mb-20 ">
         <form class="container max-w-2xl mx-auto shadow-md md:w-3/4">
           <div class="p-4 bg-gray-100 border-t-2 border-indigo-400 rounded-lg bg-opacity-5">
             <div class="max-w-sm mx-auto md:w-full md:mx-0">
@@ -15,7 +20,7 @@ const bio = () => {
                     class="mx-auto object-cover rounded-full h-16 w-16 "
                   />
                 </a> */}
-                <h1 class="text-gray-600 font-black ">
+                <h1 class="text-gray-600 font-black text-5xl ">
                   Let's Build Up Your Bio
                 </h1>
               </div>
@@ -40,22 +45,57 @@ const bio = () => {
               <h2 class="max-w-sm mx-auto md:w-1/3">Enter your Social IDs</h2>
               <div class="max-w-sm mx-auto space-y-5 md:w-2/3">
                 <div>
-                  <div class=" relative ">
+                  <div class=" relative grid grid-cols-3">
+                    <DiGithubBadge className="h-12 w-12 col-span-1" />
                     <input
-                      type="text"
-                      id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Name"
+                      type="url"
+                      id="githubID"
+                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent col-span-2"
+                      placeholder="Github Link"
                     />
                   </div>
                 </div>
                 <div>
-                  <div class=" relative ">
+                  <div class=" relative grid grid-cols-3">
+                    <AiFillLinkedin className="h-12 w-12 col-span-1" />
                     <input
-                      type="text"
-                      id="user-info-phone"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Phone number"
+                      type="url"
+                      id="linkedinID"
+                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent col-span-2"
+                      placeholder="Linkedin Link"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div class=" relative grid grid-cols-3">
+                    <AiFillTwitterCircle className="h-12 w-12 col-span-1" />
+                    <input
+                      type="url"
+                      id="twitterID"
+                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent col-span-2"
+                      placeholder="Twitter Link"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div class=" relative grid grid-cols-3">
+                    <IoMdDocument className="h-12 w-12 col-span-1" />
+                    <input
+                      type="url"
+                      id="resumeLink"
+                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent col-span-2"
+                      placeholder="Resume Link"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div class=" relative grid grid-cols-3">
+                    <CgWebsite className="h-12 w-12 col-span-1" />
+                    <input
+                      type="url"
+                      id="portfolioLink"
+                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent col-span-2"
+                      placeholder="Portfolio Link"
                     />
                   </div>
                 </div>
@@ -63,24 +103,23 @@ const bio = () => {
             </div>
             <hr />
             <div class="items-center w-full p-8 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-4/12">Change password</h2>
+              <h2 class="max-w-sm mx-auto md:w-4/12">Tell About Yourself</h2>
               <div class="w-full max-w-sm pl-2 mx-auto space-y-5 md:w-5/12 md:pl-9 md:inline-flex">
                 <div class=" relative ">
-                  <input
-                    type="text"
-                    id="user-info-password"
-                    class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Password"
-                  />
+                  <div class="col-span-2">
+                    <label class="text-gray-700" for="aboutyou">
+                      <textarea
+                        class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent resize-none	"
+                        id="aboutyou"
+                        placeholder="Tell us about yourself"
+                        name="aboutyou"
+                        rows="5"
+                        cols="40"></textarea>
+                    </label>
+                  </div>
                 </div>
               </div>
-              <div class="text-center md:w-3/12 md:pl-6">
-                <button
-                  type="button"
-                  class="py-2 px-4  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                  Change
-                </button>
-              </div>
+              <div class="text-center md:w-3/12 md:pl-6"></div>
             </div>
             <hr />
             <div class="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
